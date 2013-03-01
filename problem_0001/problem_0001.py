a13d5aa578a7a7f19e81df1e21230b0f2dce8645
arundhaj <arundhaj@gmail.com>
2013-03-01 12:13:15 +0530

    changed variable name

--- problem_0001/problem_0001.py
+++ problem_0001/problem_0001.py
@@ -3,6 +3,6 @@ print 'Project Euler Problem 0001'
 start = 0
 stop = 1000
 
-sum = sum(list(set(range(start, stop, 5) + range(start, stop, 3))))
+total = sum(list(set(range(start, stop, 5) + range(start, stop, 3))))
 
-print 'Sum is %s' % sum
+print 'Sum is %s' % total
